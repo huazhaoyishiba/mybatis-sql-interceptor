dc3642970b6a7c91051d0e0ac257b40e1d01f014
huazhaoyishiba <13087519+huazhaoyishiba@user.noreply.gitee.com>
2024-11-06 14:01:41 +0800

    优化不能打印Mybatis-plus的limit分页问题

--- src/main/java/com/huazhao/mybatis/config/SqlInterceptor.java
+++ src/main/java/com/huazhao/mybatis/config/SqlInterceptor.java
@@ -1,6 +1,7 @@
 package com.huazhao.mybatis.config;
 
 
+import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.cache.CacheKey;
 import org.apache.ibatis.executor.Executor;
@@ -17,20 +18,20 @@ import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.type.TypeHandlerRegistry;
-import org.springframework.util.ObjectUtils;
 
+import java.lang.reflect.Field;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.Properties;
 import java.util.regex.Matcher;
 
 
 @Intercepts({
         @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
         @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class, CacheKey.class, BoundSql.class}),
-        @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class})
+        @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class}),
+        @Signature(type = Executor.class, method = "delete", args = {MappedStatement.class, Object.class})
 })
 
 @Slf4j
@@ -45,12 +46,11 @@ public class SqlInterceptor implements Interceptor {
         long startTime = System.currentTimeMillis();
         Object proceed = invocation.proceed();
         long endTime = System.currentTimeMillis();
-
         String printSql = null;
         try {
             printSql = generateSql(invocation);
         } catch (Exception exception) {
-            log.error("获取sql异常", exception, RED);
+            log.warn("无法解析SQL，可能是由于动态参数问题。此日志信息并不代表执行SQL存在问题。具体异常信息：{}", exception.getMessage(), RED);
         } finally {
             long costTime = endTime - startTime;
             log.info("\n 执行SQL耗时：{}ms \n 执行SQL：{}{}", costTime, ANSI_MAGENTA, printSql, ANSI_CYAN);
@@ -60,19 +60,26 @@ public class SqlInterceptor implements Interceptor {
 
     private static String generateSql(Invocation invocation) {
         MappedStatement statement = (MappedStatement) invocation.getArgs()[0];
-        Object parameter = null;
-        if (invocation.getArgs().length > 1) {
-            parameter = invocation.getArgs()[1];
-        }
+        Object parameter = (invocation.getArgs().length > 1) ? invocation.getArgs()[1] : null;
         Configuration configuration = statement.getConfiguration();
         BoundSql boundSql = statement.getBoundSql(parameter);
+
         Object parameterObject = boundSql.getParameterObject();
         List<ParameterMapping> params = boundSql.getParameterMappings();
         String sql = boundSql.getSql();
-        sql = sql.replaceAll("[\\s]+", " ");
-        if (!ObjectUtils.isEmpty(params) && !ObjectUtils.isEmpty(parameterObject)) {
-            TypeHandlerRegistry typeHandlerRegistry = configuration.getTypeHandlerRegistry();
 
+        CacheKey cacheKey = (CacheKey) invocation.getArgs()[4];
+        List<Object> updateList = getUpdateListFromCacheKey(cacheKey);
+        if (ObjectUtils.isNotEmpty(updateList)) {
+            String str = (String) updateList.get(3);
+            if (str.length() < 12) {
+                String[] split = str.split(":");
+                sql += " LIMIT " + Integer.parseInt(split[0]) + ", " + Integer.parseInt(split[1]);
+            }
+        }
+        sql = sql.replaceAll("[\\s]+", " ");
+        if (!ObjectUtils.isEmpty(params) && parameterObject != null) {
+            TypeHandlerRegistry typeHandlerRegistry = configuration.getTypeHandlerRegistry();
             if (typeHandlerRegistry.hasTypeHandler(parameterObject.getClass())) {
                 sql = sql.replaceFirst("\\?", Matcher.quoteReplacement(getParameterValue(parameterObject)));
             } else {
@@ -82,12 +89,11 @@ public class SqlInterceptor implements Interceptor {
                     if (metaObject.hasGetter(propertyName)) {
                         Object obj = metaObject.getValue(propertyName);
                         sql = sql.replaceFirst("\\?", Matcher.quoteReplacement(getParameterValue(obj)));
-
                     } else if (boundSql.hasAdditionalParameter(propertyName)) {
                         Object obj = boundSql.getAdditionalParameter(propertyName);
                         sql = sql.replaceFirst("\\?", Matcher.quoteReplacement(getParameterValue(obj)));
                     } else {
-                        sql = sql.replaceFirst("\\?", "缺失");
+                        sql = sql.replaceFirst("\\?", "未知参数");
                     }
                 }
             }
@@ -95,26 +101,30 @@ public class SqlInterceptor implements Interceptor {
         return sql;
     }
 
-    private static String getParameterValue(Object object) {
-        String value = "";
-        if (object instanceof String) {
-            value = "'" + object.toString() + "'";
-        } else if (object instanceof Date) {
-            DateFormat format = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT, Locale.CHINA);
-            value = "'" + format.format((Date) object) + "'";
-        } else if (!ObjectUtils.isEmpty(object)) {
-            value = object.toString();
+    private static List<Object> getUpdateListFromCacheKey(CacheKey cacheKey) {
+        try {
+            Field field = CacheKey.class.getDeclaredField("updateList");
+            field.setAccessible(true);
+            return (List<Object>) field.get(cacheKey);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            return null;
         }
-        return value;
+    }
+
+    private static String getParameterValue(Object obj) {
+        if (obj instanceof String) {
+            return "'" + obj + "'";
+        } else if (obj instanceof Date) {
+            DateFormat df = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT, Locale.CHINA);
+            return "'" + df.format((Date) obj) + "'";
+        } else if (obj != null) {
+            return obj.toString();
+        }
+        return "";
     }
 
     @Override
     public Object plugin(Object target) {
         return Plugin.wrap(target, this);
-    }
-
-    @Override
-    public void setProperties(Properties properties) {
-        // 可以通过properties配置插件参数
     }
 }
